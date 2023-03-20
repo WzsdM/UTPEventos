cac6a3bb6970b54916c8ecc9af48cab91d791bb1
Yeicot JJ Sarmiento Polo <yeicotsarmientopolo2@gmail.com>
2023-03-19 19:04:23 -0500

    Frontend y Backend Juntos
    ahora se puede trabajar todo desde el mismo repositorio

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='index' ),
+    path('administrarEventos', views.adEventos, name='adEventos' ),
+    path('login', views.Login, name='login' ),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+def adEventos(request):
+    return render(request, 'admin_eventos.html')
+def Login(request):
+    return render(request, 'login.html')
